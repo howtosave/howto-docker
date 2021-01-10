2312f2adb3bc0c6e74a49f38e40e1aab7e13bc12
peterk <tiglek+t480@gmail.com>
2021-01-10 16:20:20 +0000

    fix nginx issues

--- nginx/docker-run.sh
+++ nginx/docker-run.sh
@@ -10,6 +10,9 @@ _SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT_DIR=$_SCRIPT_DIR
 _ENV=${1:-"dev"}
 
+[ ! -d "$ROOT_DIR/nginx-volume/log/nginx" ] && mkdir -p "$ROOT_DIR/nginx-volume/log/nginx"
+[ ! -d "$ROOT_DIR/nginx-volume/run/nginx" ] && mkdir -p "$ROOT_DIR/nginx-volume/run/nginx"
+
 if [ "$_ENV" == "dev" ]; then
     # run with temp container
     docker run --rm -it --name nginx-dev -p 8080:80 \
